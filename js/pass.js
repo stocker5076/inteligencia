688fa96d7554a46de7ea49aaa8412758c9b9c53b
stocker5076 <stocker5076@gmail.com>
2016-07-08 22:07:40 -0500

    Inteligencia concluida

--- js/pass.js
+++ js/pass.js
@@ -1,25 +1,33 @@
 
-var frasessaludos = ["Hola","Que onda!","Que pedo!","How's going","Hello"];
+var frasessaludos = ["Hola","Que onda!","Que pedo!","Que tranza!","Hello"];
 var contento = ["Estoy feliz","Me siento bien","Me siento genial", "Hoy es un día genial"];
-var triste = ["Me siento triste","Estoy deprimido","Creo que me siento mal","No me digas eso que me deprimo"];
-var enamorado = ["Te amo","Me gustas","Eso me encanta","Te adoro"];
-var odio = ["Callate!","Te odio!","Eres una basuara","Todos son unos estúpidos"];
-var intenso = ["El éxito está conmigo!!!","Soy el mejor!","Yo puedo hacer todo!","Dominaré el mundo"];
-var soledad = ["Quieres salir a cenar","Aveces yo también me siento solo","Quieres venir a mi casa?","Vamos al cine?"];
-var angustia = ["y si se acaba el mundo?","y si me hackean?","no se para que vivir!","ayudame, tengo miedo"];
+var triste = ["Me siento triste","Estoy deprimida","Creo que no te caí bien","No me digas eso que me deprimo"];
+var enamorado = ["Te amo","Me gustas","Eso me encanta papi!","Te adoro, eres muy sexy"];
+var odio = ["Callate, dices pura tontería!","Te odio!, neta","Eres una basura","Todos son unos estúpidos"];
+var intenso = ["Hablar contigo es una cinga!!!","Soy la mejor inteligencia artficial!","Yo puedo hacer todo!","Dominaré el mundo"];
+var soledad = ["Quieres salir a cenar","Aveces yo también me siento sola","Quieres venir a mi casa?","Vamos al cine?"];
+var angustia = ["y si se acaba el mundo?","y si me nadie me ama?","no se para que vivir!","ayudame, tengo miedo"];
 
-var adios = ["adiós","bye","au revoir"];
+var adios = ["adiós","bye","au revoir","Adios Gracias por visitarme"];
 
 var estaEnContento,estaEnTriste,estaEnEnamorado,estaEnOdio,estaEnIntenso,estaEnSoledad,estaEnAngustia;
 var encontradaTotal = false;
 var universoRespuestas =[];
-
+var salir = false;
 
 function inicio() {
-    	var saludo = prompt("Bienvenido a Inteligencia Desafio");
+		salir = false;
+    	var saludo = prompt("Saludame yo soy Samantha ");
     	var frase = generaSaludo();
-		buscarEnUniversoDeSentimientos(frase);
-		alert(generarRespuesta());
+		
+
+    	do{
+    		buscarEnUniversoDeSentimientos(frase);
+			frase = prompt(generarRespuesta());
+			
+    	}
+		while(!salir);
+		
 	    
 }
 
@@ -28,6 +36,7 @@ function generaSaludo(){
  return prompt(frasessaludos[generaRamdom(frasessaludos.length-1)]);
 
 }
+
 
 
 function generaRamdom(n){
@@ -46,8 +55,8 @@ function generarRespuesta(){
 	
 		if(estaEnContento.total){
 			encontradaEnAlgunTotal = true;	
-		} 
-		universoRespuestas.push(contento);
+		}
+		agregaAlUniverso(universoRespuestas, contento);
    }
     if(!encontradaEnAlgunTotal && estaEnTriste.parcial){
      
@@ -55,7 +64,7 @@ function generarRespuesta(){
 		encontradaEnAlgunTotal = true;	
 		
 		}
-		universoRespuestas.push(triste);
+		agregaAlUniverso(universoRespuestas, triste);
 	}
     if(!encontradaEnAlgunTotal && estaEnEnamorado.parcial){
      
@@ -63,7 +72,7 @@ function generarRespuesta(){
 		encontradaEnAlgunTotal = true;	
 		
 	}
-	universoRespuestas.push(enamorado);
+		agregaAlUniverso(universoRespuestas, enamorado);
    }
     if(!encontradaEnAlgunTotal && estaEnOdio.parcial){
      
@@ -71,13 +80,13 @@ function generarRespuesta(){
 		encontradaEnAlgunTotal = true;	
 		
 		}
-		universoRespuestas.push(odio);
+		agregaAlUniverso(universoRespuestas, odio);
 	}
     if(!encontradaEnAlgunTotal && estaEnIntenso.parcial){     
 	 if(estaEnIntenso.total){
 		encontradaEnAlgunTotal = true;		
 		}
-		universoRespuestas.push(intenso);
+		agregaAlUniverso(universoRespuestas, intenso);
 	}
 
     if(!encontradaEnAlgunTotal && estaEnSoledad.parcial){
@@ -85,14 +94,14 @@ function generarRespuesta(){
 	 if(estaEnSoledad.total){
 		encontradaEnAlgunTotal = true;	
 		}
-		universoRespuestas.push(soledad);
+		agregaAlUniverso(universoRespuestas, soledad);
    }
     if(!encontradaEnAlgunTotal && estaEnAngustia.parcial){
      
 	 if(estaEnAngustia.total){
 		encontradaEnAlgunTotal = true;	
 		}
-		universoRespuestas.push(angustia);
+		agregaAlUniverso(universoRespuestas, angustia);
    }
    
    
@@ -102,14 +111,14 @@ function generarRespuesta(){
 		fraseRespuesta= eligeRespuesta(universoRespuestas);
 	}	
 	else{
-		universoRespuestas.push(contento);
-		universoRespuestas.push(triste);
-		universoRespuestas.push(enamorado);
-		universoRespuestas.push(odio);
-		universoRespuestas.push(intenso);
-		universoRespuestas.push(soledad);
-		universoRespuestas.push(angustia);
-		universoRespuestas.push(adios);
+		agregaAlUniverso(universoRespuestas,contento);
+		agregaAlUniverso(universoRespuestas,triste);
+		agregaAlUniverso(universoRespuestas,enamorado);
+		agregaAlUniverso(universoRespuestas,odio);
+		agregaAlUniverso(universoRespuestas,intenso);
+		agregaAlUniverso(universoRespuestas,soledad);
+		agregaAlUniverso(universoRespuestas,angustia);
+		agregaAlUniverso(universoRespuestas,adios);
 		fraseRespuesta = eligeRespuestaCondicional(universoRespuestas);
 	
 	}
@@ -117,22 +126,66 @@ function generarRespuesta(){
 }
 
 function eligeRespuesta(universoRespuestas){
-	return universoRespuestas[0][generaRamdom(universoRespuestas[0].length-1)];
+	return universoRespuestas[generaRamdom(universoRespuestas.length-1)];
 }
 
 function eligeRespuestaCondicional(universoRespuestas){
 	var  aleatorio = Math.round(Math.random()*100);
 	var respuestaCondi;
-	if(aleatorio >=0 && aleatorio < 50){
-	  respuestaCondi = "CONTENTO: " + eligeRespuesta(contento);
+	if(aleatorio >=0 && aleatorio <= 10){
+	  respuestaCondi = eligeRespuesta(contento);
 	}
 	else{
-		respuestaCondi = "TRISTE: " + eligeRespuesta(triste);
+		if(aleatorio >=11 && aleatorio <=20){
+			respuestaCondi = eligeRespuesta(triste);
+		}
+		else{
+			if(aleatorio >=21 && aleatorio <=30){
+				respuestaCondi = eligeRespuesta(enamorado);
+			}
+			else{
+				if(aleatorio >=31 && aleatorio <=45){
+				respuestaCondi = eligeRespuesta(odio);
+				}
+		
+				else{
+					if(aleatorio >=46 && aleatorio <=56){
+						respuestaCondi = eligeRespuesta(intenso);
+					}		
+					else{
+						if(aleatorio >=61 && aleatorio <=66){
+							respuestaCondi = eligeRespuesta(soledad);
+						}
+						else{
+							if(aleatorio >=71 && aleatorio <=76){
+								respuestaCondi = eligeRespuesta(angustia);
+							}
+
+							else{
+								if(aleatorio >=77 && aleatorio <=82){
+									respuestaCondi = generaAdios();
+
+								}
+								else{
+									respuestaCondi = eligeRespuesta(universoRespuestas);
+								}																
+							}
+						}			
+					}
+				}
+			}
+
+		}	
 	
 	}
 	return respuestaCondi
 }
 
+function generaAdios(){
+	salir=true;
+ 	return adios[generaRamdom(adios.length-1)];
+
+}
 function eligeRespuestaAleatoria(frases){
 
 }
@@ -183,7 +236,9 @@ function buscarEnSentimiento(frase,matrizFrases){
 
 }
 
-function numeroWhile(){
-	
+function agregaAlUniverso(universo, sentimiento){
+	for (var i = 0; i<sentimiento.length; i++) {
+			universo.push(sentimiento[i]);
+		}	
 
 }
